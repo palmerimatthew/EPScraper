a5fb726c01d9d7946e6fa4a48c3ce99fa4dfd98d
palmerimatthew <37392128+palmerimatthew@users.noreply.github.com>
2019-09-11 19:30:35 -0500

    Added dependents

--- R/EP_League_Links.R
+++ R/EP_League_Links.R
@@ -14,7 +14,7 @@ EP_League_Links <- function(Data, undrafted = T) {
     gsub(' players found', '', .) %>%
     gsub(' ', '', .) %>%
     as.numeric() %>%
-    divide_by(100) %>%
+    magrittr::divide_by(100) %>%
     ceiling()
   
   all_links <- character(0)
@@ -25,7 +25,7 @@ EP_League_Links <- function(Data, undrafted = T) {
       readLines()
     links <- html %>%
       paste(collapse = '\n') %>%
-      str_match_all("<a href=\"(.*?)\"") %>%
+      stringr::str_match_all("<a href=\"(.*?)\"") %>%
       .[[1]] %>%
       .[-(1:300),2] %>%
       .[grep('player', .)]
@@ -61,4 +61,72 @@ draft_boolean <- function(website) {
     grep('Drafted', .)
   
   length(drafted) != 0
+}
+
+get_EP_Information <- function(html) {
+  
+  right_start <- html %>%
+    grep('plyr_details', .) %>%
+    as.numeric()
+  
+  right_end <- html %>%
+    grep('</section>', .) %>%
+    .[. > right_start] %>%
+    .[1] %>%
+    as.numeric()
+  
+  html[right_start:right_end]
+}
+
+get_EP_table <- function(html, Season, Need = 'Stats') {
+  
+  if(Need == 'Stats') {
+    right_start <- html %>%
+      grep('<table(.*) player-stats', .) %>%
+      .[1] %>%
+      as.numeric()
+  } else if(Need == 'Career') {
+    right_start <- html %>%
+      grep('<table(.*) total-player-stats', .) %>%
+      as.numeric()
+  } else if(Need == 'Undrafted') {
+    right_start <- html %>%
+      grep('<table(.*) goalie-stats', .) %>%
+      as.numeric()
+  }
+  
+  right_end <- html %>%
+    grep('</table>', .) %>%
+    .[. > right_start] %>%
+    .[1] %>%
+    as.numeric()
+  
+  full_table <- html %>%
+    .[right_start:right_end] %>%
+    paste(collapse = '\n') %>%
+    XML::readHTMLTable() %>%
+    .[[1]]
+  
+  if (length(full_table) == 0) {
+    full_table
+  } else if (Season == 'R') {
+    full_table %>%
+      .[,-(10:ncol(.))]
+    
+  } else if (Season == 'P') {
+    full_table %>%
+      .[, -(4:10)]
+    
+  } else if (Season == 'RP') {
+    regularseason_table <- full_table %>%
+      .[,-(10:ncol(.))] %>%
+      dplyr::mutate(Regular_Playoffs = 'Regular')
+    playoff_table <- full_table %>%
+      .[, -(4:11)] %>%
+      dplyr::mutate(Regular_Playoffs = 'Playoffs')
+    rbind(regularseason_table, playoff_table)
+  } else {
+    full_table
+  }
+  
 }
