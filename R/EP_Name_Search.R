a7b354bf74f97544600539aac42fcedfadb38b3f
palmerimatthew <37392128+palmerimatthew@users.noreply.github.com>
2019-09-20 20:54:21 -0500

    added include_db parameter

--- R/EP_Name_Search.R
+++ R/EP_Name_Search.R
@@ -2,10 +2,11 @@
 #'
 #' Returns a list of URLs of the players from a league stats URL.
 #' @param Name A string of the desired player's name
+#' @param include_db Boolean to determine if you also want to return birthdate information alongside the links
 #' @return A vector of player eliteprospects URLs with the same name
 #' @export
 
-EP_Name_Search <- function(Name) {
+EP_Name_Search <- function(Name, include_db = F) {
   website <- paste0('https://www.eliteprospects.com/search/player?q=', gsub(' ', '+', Name))
   html <- website %>%
     readLines()
@@ -20,11 +21,25 @@ EP_Name_Search <- function(Name) {
     .[1] %>%
     as.numeric()
   
-  html %>%
+  Links <- html %>%
     .[start:end] %>%
     paste(collapse = '\n') %>%
     stringr::str_match_all("<a href=\"(.*?)\"") %>%
     .[[1]] %>%
     .[, 2] %>%
     .[grep('/player/', .)]
+  
+  if(include_db) {
+    Birth_Date <- html %>%
+      .[start:end] %>%
+      XML::readHTMLTable() %>%
+      .[[1]] %$%
+      Born %>%
+      as.character() %>%
+      gsub('\n(.*)', '', .)
+    
+    data.frame(Links, Birth_Date)
+  } else {
+    Links
+  }
 }
